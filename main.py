41361eed56be4da024b95b449d8aee6b13a6b985
LAKIQ <104293375+LAKIQ@users.noreply.github.com>
2023-01-30 18:10:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ st.title("ทดสอบกล้อง")
 
 webrtc_streamer(key="test",
                 media_stream_constraints={"video": True,"audio": False})
+
+
+run_btn = st.button("Run")
+if run_btn:
+    st.markdown("Button has already clicked") 
+
+age_inp = st.number_input("Input")
+st.markdown(f"Your input is {age_inp}")             
