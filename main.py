2401fd04e7c36782e9d12986140bc546c6702687
LAKIQ <104293375+LAKIQ@users.noreply.github.com>
2023-01-30 17:57:38 +0700

    Update main.py
    feat :+1:  : เพิ่มฟังก์ชันเปิดกล้อง

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import streamlit as st 
+from streamlit_webrtc import webrtc_streamer
 import cv2
 
-st.set_page_config(page_title="สวัสดีทุกคน")
+st.set_page_config(page_title="โปรเจคตรวจจับยาแผง")
 
-st.text("การประมวลผลภาพ")
+st.title("ตรวจ ยาแผง")
+
+st.markdown("Code for Open camera")
+code = '''import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+st.title("ทดสอบกล้อง")
+
+webrtc_streamer(key="test",
+                media_stream_constraints={"video": True,"audio": False})'''
+st.code(code , language='python')
+
+st.title("ทดสอบกล้อง")
+
+webrtc_streamer(key="test",
+                media_stream_constraints={"video": True,"audio": False})
